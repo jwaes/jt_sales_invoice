cf517b283acc578919aa47e5654f0237a1a0f45a
jwaes <jaco.waes@gmail.com>
2023-02-21 14:04:01 +0000

    delivery status

--- models/sale_order.py
+++ models/sale_order.py
@@ -14,6 +14,7 @@ class SaleOrder(models.Model):
 
     ], default='not overruled', tracking=True, string='Invoice status overruled')
 
+
     @api.depends('invoice_status_overruled', 'state', 'order_line.invoice_status')
     def _get_invoice_status(self):
         super()._get_invoice_status()
@@ -22,4 +23,32 @@ class SaleOrder(models.Model):
             if order.invoice_status_overruled != 'not overruled':
                 _logger.info("Sale order invoice status overruling from [%s] -> [%s]", order.invoice_status, order.invoice_status_overruled)
                 order.invoice_status = order.invoice_status_overruled
+
+
+    delivery_status = fields.Selection([
+        ('not', 'Not Delivered'),
+        ('partial', 'Partial Delivery'),
+        ('delivered', 'All delivered'),
+        ('na', 'Not applicable')
+    ], string='Delivery Status', compute='_compute_delivery_status')
+
+    @api.depends('picking_ids', 'picking_ids.state')
+    def _compute_delivery_status(self):
+        for so in self:
+            delivered_count = 0
+            not_delivered_count = 0
+            for picking in so.picking_ids:
+                if picking.state == 'done':
+                    delivered_count += 1
+                elif picking.state not in ('draft', 'cancel'):
+                    not_delivered_count += 1
+            if delivered_count > 0:
+                if not_delivered_count == 0:
+                    so.delivery_status = 'delivered'
+                elif not_delivered_count > 0:
+                    so.delivery_status = 'partial'
+            elif not_delivered_count > 0:
+                so.delivery_status = 'not'
+            else :
+                so.delivery_status = 'na'
 
