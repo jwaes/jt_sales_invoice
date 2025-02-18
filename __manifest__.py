543826b029a68bab196d0852b2c21ebd51503c68
jwaes <jaco.waes@gmail.com>
2025-02-18 11:53:40 +0000

    re-enable views

--- __manifest__.py
+++ __manifest__.py
@@ -15,19 +15,14 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '18.0.1.0.0',
+    'category': 'Customizations',
+    'version': '18.0.1.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base','sale', 'sale_stock'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        # 'views/sale_order_views.xml',
+        'views/sale_order_views.xml',
     ],
-    # only loaded in demonstration mode
-    # 'demo': [
-    #     'demo/demo.xml',
-    # ],
 }
